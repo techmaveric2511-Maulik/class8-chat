7bcaad8f979e2fa96bf6c0803996c67c1bd8376f
techmaveric2511-Maulik <techmaveric2511@gmail.com>
2026-03-02 09:38:19 +0530

    Implement starfield animation in app.js
    Added starfield animation to the canvas.

--- app.js
+++ app.js
@@ -94,3 +94,33 @@ usersList.appendChild(div);
 });
 });
 }
+// 🌠 STARFIELD ANIMATION
+const canvas=document.getElementById("stars");
+const ctx=canvas.getContext("2d");
+
+canvas.width=window.innerWidth;
+canvas.height=window.innerHeight;
+
+let stars=[];
+for(let i=0;i<200;i++){
+stars.push({
+x:Math.random()*canvas.width,
+y:Math.random()*canvas.height,
+size:Math.random()*2
+});
+}
+
+function animateStars(){
+ctx.clearRect(0,0,canvas.width,canvas.height);
+ctx.fillStyle="white";
+stars.forEach(s=>{
+ctx.beginPath();
+ctx.arc(s.x,s.y,s.size,0,Math.PI*2);
+ctx.fill();
+s.y+=0.5;
+if(s.y>canvas.height) s.y=0;
+});
+requestAnimationFrame(animateStars);
+}
+
+animateStars();
